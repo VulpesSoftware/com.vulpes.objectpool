258bb011fe79b194e25e7b91ee74d0907209a0e4
Vulpes Software <tylahheil@gmail.com>
2023-01-21 13:49:38 +1000

    CHANGE: Minor refactors and bug fixes.

--- Scripts/Runtime/Core/Pool.cs
+++ Scripts/Runtime/Core/Pool.cs
@@ -9,76 +9,71 @@ namespace Vulpes.Pooling
     {
         private const int DEFAULT_POOL_SIZE = 8;
         
-        private Dictionary<int, Queue<GameObject>> poolDictionary = new Dictionary<int, Queue<GameObject>>();
-        private Dictionary<int, int> keyDictionary = new Dictionary<int, int>();
+        private readonly Dictionary<int, Queue<GameObject>> poolDictionary = new();
+        private readonly Dictionary<int, int> keyDictionary = new();
 
         [SerializeField] private bool destroyUnpooledObjects = false;
 
         public static Pool Instance { get; private set; }
 
         private void Awake()
-        {
-            Instance = this as Pool;
-        }
+            => Instance = this;
 
-        public static void Add(GameObject akPrefab, int aiCount = DEFAULT_POOL_SIZE)
+        /// <summary>
+        /// Adds a prefab to the pooling system, if the added prefab already exists and contains inactive instances it will expand the pool by the specified count.
+        /// </summary>
+        public static void Add(GameObject prefab, in int count = DEFAULT_POOL_SIZE)
         {
-            int poolKey = akPrefab.GetInstanceID();
+            int poolKey = prefab.GetInstanceID();
             if (!Instance.poolDictionary.ContainsKey(poolKey))
             {
-                Instance.poolDictionary.Add(poolKey, new Queue<GameObject>());
+                Instance.poolDictionary.Add(poolKey, new());
             } else
             {
                 int availableCount = Instance.poolDictionary[poolKey].Count;
                 if (availableCount == 0)
                 {
-                    Debug.LogWarning(string.Format(
-                        "Vulpes.Pooling.Pool.Add: Attempted to Add '{0} ({1}) but there is already a known Pool with that ID, expanding Pool by {2}.",
-                        akPrefab.name,
-                        akPrefab.GetInstanceID(),
-                        aiCount.ToString()));
+                    Debug.LogWarning($"{Instance.GetType()}.Add: Attempted to Add '{prefab.name} ({prefab.GetInstanceID()}) but there is already a known Pool with that ID, expanding Pool by {count}.");
                 } else
                 {
-                    Debug.LogWarning(string.Format(
-                        "Vulpes.Pooling.Pool.Add: Attempted to Add '{0} ({1}) but there is already a known Pool with that ID containing {2} inactive instances.",
-                        akPrefab.name,
-                        akPrefab.GetInstanceID(),
-                        availableCount.ToString()));
+                    Debug.LogWarning($"{Instance.GetType()}.Add: Attempted to Add '{prefab.name} ({prefab.GetInstanceID()}) but there is already a known Pool with that ID containing {availableCount} inactive instances");
                     return;
                 }
             }
-            if (aiCount <= 0)
+            if (count <= 0)
             {
-                Debug.LogWarning("Vulpes.Pooling.Pool.Add: Pool Add Count must be greater than or equal to one!");
-                aiCount = 1;
+                Debug.LogError($"{Instance.GetType()}.Add: Pool Add Count must be greater than or equal to one!");
+                return;
             }
-            for (int i = aiCount - 1; i >= 0; i--)
+            for (int i = count - 1; i >= 0; i--)
             {
-                GameObject newObject = Instantiate(akPrefab) as GameObject;
-                Instance.keyDictionary.Add(newObject.GetInstanceID(), akPrefab.GetInstanceID());
+                GameObject newObject = Instantiate(prefab);
+                Instance.keyDictionary.Add(newObject.GetInstanceID(), prefab.GetInstanceID());
                 newObject.SetActive(false);
                 newObject.transform.SetParent(Instance.transform);
                 Instance.poolDictionary[poolKey].Enqueue(newObject);
             }
         }
 
-        public static void Add<T>(T akPrefab, int aiCount = DEFAULT_POOL_SIZE) where T : Component
-        {
-            Add(akPrefab.gameObject, aiCount);
-        }
+        /// <summary>
+        /// Adds a prefab to the pooling system, if the added prefab already exists and contains inactive instances it will expand the pool by the specified count.
+        /// </summary>
+        public static void Add<T>(T prefab, in int count = DEFAULT_POOL_SIZE) 
+            where T : Component
+            => Add(prefab.gameObject, count);
 
-        public static void Remove(GameObject akPrefab)
+        /// <summary>
+        /// Removes a prefab from the pooling system if it exists and cleans up all instances of that prefab.
+        /// </summary>
+        public static void Remove(GameObject prefab)
         {
-            int poolKey = akPrefab.GetInstanceID();
+            int poolKey = prefab.GetInstanceID();
             if (!Instance.poolDictionary.ContainsKey(poolKey))
             {
-                Debug.LogWarning(string.Format(
-                    "Vulpes.Pooling.Pool.Remove: Attempted to Remove Pool for '{0} ({1}), but there is no known Pool with that ID.",
-                    akPrefab.name,
-                    akPrefab.GetInstanceID()));
+                Debug.LogWarning($"{Instance.GetType()}.Remove: Attempted to Remove Pool for '{prefab.name} ({prefab.GetInstanceID()}), but there is no known Pool with that ID.");
                 return;
             }
-            List<GameObject> cleanup = new List<GameObject>();
+            List<GameObject> cleanup = new();
             foreach (GameObject obj in Instance.poolDictionary[poolKey])
             {
                 cleanup.Add(obj);
@@ -92,33 +87,34 @@ namespace Vulpes.Pooling
             }
         }
 
-        public static void Remove<T>(T akPrefab) where T : Component
+        /// <summary>
+        /// Removes a prefab from the pooling system if it exists and cleans up all instances of that prefab.
+        /// </summary>
+        public static void Remove<T>(T prefab) 
+            where T : Component
+            => Remove(prefab.gameObject);
+
+        /// <summary>
+        /// Spawns and returns an instance of the specified prefab.
+        /// </summary>
+        public static GameObject Spawn(GameObject prefab, Vector3 position = default, Quaternion rotation = default, Transform parent = null)
         {
-            Remove(akPrefab.gameObject);
-        }
-        
-        public static GameObject Spawn(GameObject akPrefab, Vector3 avPosition = default, Quaternion aqRotation = default, Transform akParent = null)
-        {
-            int poolKey = akPrefab.GetInstanceID();
+            int poolKey = prefab.GetInstanceID();
             bool containsKey = Instance.poolDictionary.ContainsKey(poolKey);
             if (!containsKey || Instance.poolDictionary[poolKey].Count == 0)
             {
                 if (!containsKey)
                 {
-                    Debug.LogWarning(string.Format(
-                        "Vulpes.Pooling.Pool.Spawn: Attempted to Spawn '{0} ({1}), but there is no known Pool with that ID, a new Pool will be created for this instance.",
-                        akPrefab.name,
-                        akPrefab.GetInstanceID()));
+                    Debug.LogWarning($"{Instance.GetType()}.Spawn: Attempted to Spawn '{prefab.name} ({prefab.GetInstanceID()}), but there is no known Pool with that ID, a new Pool will be created for this instance.");
                 }
-                Add(akPrefab);
+                Add(prefab);
             }
             GameObject newObject = Instance.poolDictionary[poolKey].Dequeue();
-            newObject.transform.position = avPosition;
-            newObject.transform.rotation = aqRotation;
+            newObject.transform.SetPositionAndRotation(position, rotation);
             newObject.SetActive(true);
             newObject.transform.SetParent(null);
-            SceneManager.MoveGameObjectToScene(newObject, akParent != null ? akParent.gameObject.scene : SceneManager.GetActiveScene());
-            newObject.transform.SetParent(akParent);
+            SceneManager.MoveGameObjectToScene(newObject, parent != null ? parent.gameObject.scene : SceneManager.GetActiveScene());
+            newObject.transform.SetParent(parent);
             ISpawnable[] spawnables = newObject.GetComponents<ISpawnable>();
             for (int i = spawnables.Length - 1; i >= 0; i--)
             {
@@ -127,44 +123,47 @@ namespace Vulpes.Pooling
             return newObject;
         }
 
-        public static T Spawn<T>(T akPrefab, Vector3 avPosition = default, Quaternion aqRotation = default, Transform akParent = null) where T : Component
-        {
-            return Spawn(akPrefab.gameObject, avPosition, aqRotation, akParent).GetComponent<T>();
-        }
+        /// <summary>
+        /// Spawns and returns an instance of the specified prefab.
+        /// </summary>
+        public static T Spawn<T>(T prefab, Vector3 position = default, Quaternion rotation = default, Transform parent = null) 
+            where T : Component
+            => Spawn(prefab.gameObject, position, rotation, parent).GetComponent<T>();
 
-        public static void Despawn(GameObject akObject)
+        /// <summary>
+        /// Despawns an instance of the specified prefab and returns it to the pool.
+        /// </summary>
+        public static void Despawn(GameObject instance)
         {
-            ISpawnable[] spawnables = akObject.GetComponents<ISpawnable>();
+            ISpawnable[] spawnables = instance.GetComponents<ISpawnable>();
             for (int i = spawnables.Length - 1; i >= 0; i--)
             {
                 spawnables[i].OnDespawn();
             }
-            if (!Instance.keyDictionary.TryGetValue(akObject.GetInstanceID(), out int poolKey))
+            if (!Instance.keyDictionary.TryGetValue(instance.GetInstanceID(), out int poolKey))
             {
-                Debug.LogWarning(string.Format(
-                    "Vulpes.Pooling.Pool.Despawn: Attempted to Despawn '{0} ({1}), but it is not a member of any known Pool, object reference will be {2} instead.",
-                    akObject.name,
-                    akObject.GetInstanceID(),
-                    Instance.destroyUnpooledObjects ? "destroyed" : "made inactive"));
+                Debug.LogWarning($"{Instance.GetType()}.Despawn: Attempted to Despawn '{instance.name} ({instance.GetInstanceID()}), but it is not a member of any known Pool, object reference will be {(Instance.destroyUnpooledObjects ? "destroyed" : "made inactive")} instead.");
                 if (Instance.destroyUnpooledObjects)
                 {
-                    Destroy(akObject);
+                    Destroy(instance);
                 } else
                 {
-                    akObject.SetActive(false);
+                    instance.SetActive(false);
                 }
                 return;
             }
-            akObject.SetActive(false);
-            akObject.transform.SetParent(null);
-            SceneManager.MoveGameObjectToScene(akObject, Instance.gameObject.scene);
-            akObject.transform.SetParent(Instance.transform);
-            Instance.poolDictionary[poolKey].Enqueue(akObject);
+            instance.SetActive(false);
+            instance.transform.SetParent(null);
+            SceneManager.MoveGameObjectToScene(instance, Instance.gameObject.scene);
+            instance.transform.SetParent(Instance.transform);
+            Instance.poolDictionary[poolKey].Enqueue(instance);
         }
 
-        public static void Despawn<T>(T akObject) where T : Component
-        {
-            Despawn(akObject.gameObject);
-        }
+        /// <summary>
+        /// Despawns an instance of the specified prefab and returns it to the pool.
+        /// </summary>
+        public static void Despawn<T>(T instance) 
+            where T : Component
+            => Despawn(instance.gameObject);
     }
 }
--- Scripts/Runtime/Interfaces/ISpawnable.cs
+++ Scripts/Runtime/Interfaces/ISpawnable.cs
@@ -2,7 +2,14 @@
 {
     public interface ISpawnable 
     {
+        /// <summary>
+        /// Called whenever an instance is spawned.
+        /// </summary>
         void OnSpawn();
+
+        /// <summary>
+        /// Called whenever an instance is despawned.
+        /// </summary>
         void OnDespawn();
     }
 }
